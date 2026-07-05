c683defd91cc9b26787ca96f0db767f159f26a0f
Carlos Eduardo da Silva Papa <papa.cesp@gmail.com>
2026-07-05 12:38:26 -0300

    +

---
+++ .filter/dFilter.py
@@ -0,0 +1,12 @@
+DEFAULT_CONFIG: dict = {
+    "ignored_dirs": [
+        "__pycache__", ".git", "node_modules", 
+        ".vscode", "toolbox", ".venv", "tests"
+        ".claude", ".codex",
+        ],
+    "visible_dirs": [],
+    "ignored_extensions": [".txt", ],
+    "visible_extensions": [],
+    "ignored_files": ["caminhos.txt", "tree_focada.txt"],
+    "visible_files": [],
+}
---
+++ .filter/zFilter.py
@@ -0,0 +1,21 @@
+DEFAULT_CONFIG: dict = {
+    "ignored_dirs": [
+        ".vscode",
+        ".claude",
+        ".codex",
+        ".git",
+        ".filter",
+        ".docs"
+        ],
+    "visible_dirs": [
+        '.log',
+        "assets"
+    ],
+    "ignored_extensions": [
+        ".jpeg",
+        ".png",
+    ],
+    "visible_extensions": [],
+    "ignored_files": ["caminhos.txt", "tree_focada.txt"],
+    "visible_files": [],
+}
