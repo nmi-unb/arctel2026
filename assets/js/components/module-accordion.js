ceba19cc939db10e3b19a549a85d131fe15e0169
Cadu-Tower <papa.cesp@gmail.com>
2026-07-14 20:44:52 -0300

    +

--- assets/js/components/module-accordion.js
+++ assets/js/components/module-accordion.js
@@ -5,8 +5,16 @@ export function initModuleAccordion() {
     const toggle = module.querySelector(".module__toggle");
     toggle.addEventListener("click", () => {
       const isOpen = module.classList.contains("is-open");
-      modules.forEach((m) => m.classList.remove("is-open"));
-      if (!isOpen) module.classList.add("is-open");
+
+      modules.forEach((m) => {
+        m.classList.remove("is-open");
+        m.querySelector(".module__toggle").setAttribute("aria-expanded", "false");
+      });
+
+      if (!isOpen) {
+        module.classList.add("is-open");
+        toggle.setAttribute("aria-expanded", "true");
+      }
     });
   });
 }
