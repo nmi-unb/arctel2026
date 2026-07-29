bb4cf9c8ea498fce5eb334f7b0fff0543965f7af
Carlos Eduardo da Silva Papa <papa.cesp@gmail.com>
2026-07-29 11:49:41 -0300

    +

--- _docs/dFilter.py
+++ _docs/dFilter.py
@@ -1,5 +1,5 @@
 CONFIG_SOURCE: dict = {
-    "ignored_dirs": [""],
+    "ignored_dirs": ["_privado"],
     "visible_dirs": ["assets", "modulos"],
     "ignored_extensions": [],
     "visible_extensions": [],
